622d42495d1a31f6f787a66b46cb6defc4ad439b
dmaaj <38289885+dmaaj@users.noreply.github.com>
2018-04-11 15:37:35 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+ <link rel="stylesheet" href="http://localhost/figma/styles.css"> 
+</head>
+
+<body>
+	<?php date_default_timezone_set('Africa/Lagos');?>
+	<div class="header">
+		<div class="logo"</div>
+		<div class="logotext"> HNGINTERNSHIP
+		<div class= "menu1"> HOME</div>
+		<div class= "menu2"> ABOUT</div>
+		<div class= "menu3"> CONTACT</div></div>
+	</div>
+	<div class="slider"></div>
+	<div class="body">
+	<div id="txt1" class="text"><?php echo date("H");?></div>	
+	<div id="txt2" class="text"><?php echo date("i");?></div>	
+	<div id="txt3" class="text"><?php echo date("s");?></div>	
+	</div>
+	
+	<div class="footer">
+		
+		<div class="footer-text">Copyright All Rights Reserved © 2018</div>
+	</div>
+
+</body>
+</html>
